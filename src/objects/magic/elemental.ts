fa964a1e00831793ecbd83145a7bc6a17fd952ea
Andrew Cox <roo@rhmg.co.uk>
2021-03-18 14:57:45 +0200

    fixes elemental trait mistake

--- src/objects/magic/elemental.ts
+++ src/objects/magic/elemental.ts
@@ -1,5 +1,5 @@
 import { Armour, Weapon } from "..";
-import { Move, Physicality, Dexterity, Constitution, Mind, Trait, EquipmentProperty } from "../../defs";
+import { Move, Physicality, Dexterity, Constitution, Mind, Trait, EquipmentProperty, CharacterClass, CharacterClasses } from "../../defs";
 import { Moveable, Keyed, Physical } from "../../interfaces";
 
 export class Elemental implements Moveable, Keyed {
@@ -16,8 +16,9 @@ export class Elemental implements Moveable, Keyed {
   DEX: Dexterity = new Dexterity();
   CON: Constitution = new Constitution();
   MND: Mind = new Mind();
+  private _characterClass = CharacterClass.Instinct();
 
-  private _traits: Trait[] = [ Trait.Instinct() ];
+  private _traits: Trait[] = [];
   get Traits(): Trait[] { return this._traits; };
   private _weapons: Weapon[] = [];
   get Weapons(): Weapon[] { return this._weapons; }
@@ -28,29 +29,19 @@ export class Elemental implements Moveable, Keyed {
     return 0;
   }
 
+  
+  SetCharacterClass(characterClass: CharacterClass) : Elemental {
+    this._characterClass = characterClass;
+    return this;
+  }
+
   AddTrait(trait: Trait) : Elemental {
-    if (trait.Key === 'Instinct') {
-      if (this._traits.find((t) => t.Key === 'Regular')) {
-        this._traits = this._traits.filter((t) => t.Key !== 'Regular');
-      }
-    }
-    if (trait.Key === 'Regular') {
-      if (this._traits.find((t) => t.Key === 'Instinct')) {
-        this._traits = this._traits.filter((t) => t.Key !== 'Instinct');
-      }
-    }
     this._traits.push(trait);
     trait.AddEffect(this);
     return this;
   }
 
   RemoveTrait(trait: Trait) : Elemental {
-    if (trait.Key === 'Instinct') {
-      this._traits.push(Trait.Regular());
-    }
-    if (trait.Key === 'Regular') {
-      this._traits.push(Trait.Instinct());
-    }
     this._traits = this._traits.filter((t) => t.Key !== trait.Key);
     trait.RemoveEffect(this);
     return this;
@@ -149,7 +140,7 @@ export class Elemental implements Moveable, Keyed {
 
   static Skeleton(weaponText: string): Elemental {
     const result = new Elemental('Skeleton')
-    .AddTrait(Trait.Regular())
+    .SetCharacterClass(CharacterClass.Regular())
     .AddWeapon(new Weapon(weaponText, 1, 3));
     result.MOV.Value = 4;
     result.PHY.Value = 1;
